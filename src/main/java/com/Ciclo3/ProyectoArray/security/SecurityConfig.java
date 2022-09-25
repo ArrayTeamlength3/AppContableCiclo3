40a5e47647d11eeeae7bb1dcc8b75fa2c3119e24
ArrayTeamlength3 <arrayteam.length3@gmail.com>
2022-09-24 23:47:22 -0500

    TRUCO

--- src/main/java/com/Ciclo3/ProyectoArray/security/SecurityConfig.java
+++ src/main/java/com/Ciclo3/ProyectoArray/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/","/VerEmpresas/**").hasRole("ADMIN")
+                .antMatchers("/","VerEmpresas/**").hasRole("ADMIN")
                 .antMatchers("/AgregarEmpresa/**").hasRole("ADMIN")
                 .antMatchers("/GuardarEmpresa/**").hasRole("ADMIN")
                 .antMatchers("/EditarEmpresa/**").hasRole("ADMIN")
